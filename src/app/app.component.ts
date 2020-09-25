f20e806829c271765a717a3efaa4c3db0452a074
Jhayr <dmz.gfx@gmail.com>
2020-09-25 17:00:06 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { CalendarOptions } from '@fullcalendar/angular';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'appetiser-calendar-frontend';
+  
+  eventForm: FormGroup;
+  isLoading: boolean = false;
+
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth'
+  };
+
+  daysName = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
+  constructor(private http: HttpClient) {
+    this.eventForm = new FormGroup({
+      event_name: new FormControl(null, [Validators.required]),
+      date_from: new FormControl(null, [Validators.required]),
+      date_to: new FormControl(null, [Validators.required]),
+      days: new FormArray([
+        new FormControl(false),
+        new FormControl(false),
+        new FormControl(false),
+        new FormControl(false),
+        new FormControl(false),
+        new FormControl(false),
+        new FormControl(false)
+      ])
+    });
+  }
+
+  get days() {
+    return this.eventForm.get('days');
+  }
+
+  submit(form: FormGroup) {
+    if(this.isLoading || form.invalid) {
+      return false;
+    }
+
+    this.isLoading = true;
+
+    this.http.post(environment.apiUrl + '/events', form.value)
+      .subscribe(res => {
+        console.log(res);
+
+        this.calendarOptions.events = res;
+
+        this.isLoading = false;
+      });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import dayGridPlugin from '@fullcalendar/daygrid';
+
+FullCalendarModule.registerPlugins([
+  dayGridPlugin
+]);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule,
+    FullCalendarModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatDatepickerModule,
+    MatMomentDateModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
